ccac5ed1136c071a649ded2edda6ad986970bea1
sergiomeneguzzo <sergio03.mene@gmail.com>
2025-02-04 09:59:21 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        cookiePolicy: resolve(__dirname, 'public/legal/cookie-policy.html'),
+        cookiePolicy: resolve(__dirname, 'legal/cookie-policy.html'),
       },
     },
     cssCodeSplit: true,
